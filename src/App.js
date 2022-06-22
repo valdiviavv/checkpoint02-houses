00c1d8434d5f6a3de8480cf1277589b49209f54a
veronica <valdiviavv@hotmail.com>
2022-06-22 19:04:03 -0400

    The App component was implemented.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Nav from './components/Nav/Nav';
+import Houses from "./components/Houses/Houses";
+import HouseDetail from "./components/HouseDetail/HouseDetail";
+import CreateHouse from "./components/CreateHouse/CreateHouse";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-};
+    return (
+        <div className="App">
+            <Nav />
+            <Route exact path="/" component={Houses}/>
+            <Route path="/houses/:houseId" component={HouseDetail} />
+            <Route path="/house/create/" component={CreateHouse}/>
+        </div>
+    );
+}
 
 export default App;

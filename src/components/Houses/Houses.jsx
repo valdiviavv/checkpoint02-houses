8141caf89d0dfbec81d9d695d9159ed31093b039
veronica <valdiviavv@hotmail.com>
2022-06-23 19:25:13 -0400

    There are 2 failing and 53 passed tests from 55 in total. The 2 failing tests belongs to Houses component. Maybe the unit test is the problem.

--- src/components/Houses/Houses.jsx
+++ src/components/Houses/Houses.jsx
@@ -7,6 +7,11 @@ import {HouseCard} from "../HouseCard/HouseCard";
 // CUIDADOOOO. SI O SI CLASS COMPONENT! SE ROMPEN LOS TEST EN CASO CONTRARIO!!
 // TAMBIEN VAS A TENER QUE USAR EL METODO CONNECT DE REDUX, JUNTO A MAP_STATE_TO_PROPS 
 // Y MAP_DISPATCH_TO_PROPS!! <3
+/*
+After create house this page is rendered. The 4th house arrives in the store.
+But the componentDidMount() calls the method getAllHouses() which calls the API
+and the store is updated with only 3 houses and the 4th one is lost.
+ */
 export class Houses extends Component {
 
     componentDidMount() {
@@ -19,7 +24,8 @@ export class Houses extends Component {
                 <h1>Game of Thrones</h1>
                 <img src={main_image_cp2} alt={'main-img'}/>
                 <h3>Houses</h3>
-                {this.props.houses?.map(house =>
+                {this.props.houses &&
+                    this.props.houses.map(house =>
                     <HouseCard
                         key={house.id}
                         id={house.id}
--- src/index.js
+++ src/index.js
@@ -15,3 +15,21 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+/*
+Test Results:
+Test Suites: 1 failed, 8 passed, 9 total
+Tests:       2 failed, 53 passed, 55 total
+
+  <Houses />
+    ✓ Debería rederizar un "h1" con el texto "Game of Thrones" (31 ms)
+    ✓ Debería renderizar en un tag "img" la imagen provista en la carpeta "img-cp2" (3 ms)
+    ✓ La imagen debería tener un atributo "alt" con el texto "main-img" (3 ms)
+    ✓ Debería rederizar un "h3" con el texto "Houses" (2 ms)
+    connect Redux
+      ✓ Debería traer de redux nuestras houses usando mapStateToProps (3 ms)
+      ✓ Debería traer por props la action creator getAllHouses de Redux usando mapDispatchToProps (3 ms)
+    React LifeCycles
+      ✕ Debería mapear la cantidad de houses que hayan en el store y renderizar una <HouseCard /> por cada una (11 ms)
+      ✕ Debería pasar como props a cada componente <HouseCard /> las propiedades id, region, name y words de cada house (6 ms)
+ */
